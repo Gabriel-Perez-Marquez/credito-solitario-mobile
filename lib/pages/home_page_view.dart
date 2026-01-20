1798c54a213ac80378e07e454fcae0eeebad30c0
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2026-01-20 13:30:45 +0100

    Primer diseño de la pagina de inicio de sesión hecho

---
+++ lib/pages/home_page_view.dart
@@ -0,0 +1,20 @@
+import 'package:credito_solitario_mobile/shared/login.dart';
+import 'package:flutter/material.dart';
+
+class HomePageView extends StatelessWidget {
+  const HomePageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            Login()
+          ],
+        ),
+      ),
+    );
+  }
+}
